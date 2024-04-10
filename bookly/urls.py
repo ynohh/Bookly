853933340c2795ee39c2095ec16c91fb153dd98b
Yago Leão <yagoleao@mx2.unisc.br>
2024-04-10 18:27:30 -0300

    Delete bookly directory

--- bookly/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = "bookly"
-urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("", views.ListView.as_view(), name="list"),
-]
